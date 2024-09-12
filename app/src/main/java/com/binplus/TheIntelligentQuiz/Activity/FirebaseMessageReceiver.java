d7fee33d843dbc739bc2e7f24379f0ec34289921
ASUS <vaidik.nigam.jhs@gmail.com>
2024-09-12 14:40:57 +0530

    Volley Api Completed

--- app/src/main/java/com/binplus/TheIntelligentQuiz/Activity/FirebaseMessageReceiver.java
+++ app/src/main/java/com/binplus/TheIntelligentQuiz/Activity/FirebaseMessageReceiver.java
@@ -1,25 +1,27 @@
 package com.binplus.TheIntelligentQuiz.Activity;
 
+import android.Manifest;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Build;
 import android.util.Log;
 import android.widget.RemoteViews;
 
 import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
 import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
-import com.binplus.TheIntelligentQuiz.Activity.MainActivity;
 import com.binplus.TheIntelligentQuiz.R;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
-public class FirebaseMessageReceiver
-        extends FirebaseMessagingService {
-
+public class FirebaseMessageReceiver extends FirebaseMessagingService {
+NotificationManager notificationManager;
     // Override onNewToken to get new token
     @Override
     public void onNewToken(@NonNull String token)
@@ -33,6 +35,10 @@ public class FirebaseMessageReceiver
     public void
     onMessageReceived(RemoteMessage remoteMessage)
     {
+        Log.e("dxcfbhj","fghjff");
+        Log.e("dcfvghj",remoteMessage.getData().toString());
+        Log.e("dcfvghj",remoteMessage.getNotification().getBody());
+        Log.e("dcfvghj",remoteMessage.getNotification().getTitle());
         // First case when notifications are received via
         // data event
         // Here, 'title' and 'message' are the assumed names
@@ -47,13 +53,28 @@ public class FirebaseMessageReceiver
 
         // Second case when notification payload is
         // received.
+
+        NotificationManager notificationManager
+                = (NotificationManager)getSystemService(
+                Context.NOTIFICATION_SERVICE);
+        // Check if the Android Version is greater than Oreo
+        if (Build.VERSION.SDK_INT
+                >= Build.VERSION_CODES.O) {
+            NotificationChannel notificationChannel
+                    = new NotificationChannel(
+                    "notification_channel", "web_app",
+                    NotificationManager.IMPORTANCE_HIGH);
+            notificationManager.createNotificationChannel(
+                    notificationChannel);
+        }
+
         if (remoteMessage.getNotification() != null) {
             // Since the notification is received directly
             // from FCM, the title and the body can be
             // fetched directly as below.
             showNotification(
-                    remoteMessage.getNotification().getTitle(),
-                    remoteMessage.getNotification().getBody());
+                remoteMessage.getNotification().getTitle(),
+                remoteMessage.getNotification().getBody());
         }
     }
 
@@ -63,12 +84,12 @@ public class FirebaseMessageReceiver
                                         String message)
     {
         RemoteViews remoteViews = new RemoteViews(
-                getApplicationContext().getPackageName(),
-                R.layout.custom_notification);
+            getApplicationContext().getPackageName(),
+            R.layout.custom_notification);
         remoteViews.setTextViewText(R.id.title, title);
         remoteViews.setTextViewText(R.id.message, message);
         remoteViews.setImageViewResource(R.id.icon,
-                R.drawable.savings);
+                                         R.drawable.savings);
         return remoteViews;
     }
 
@@ -78,8 +99,12 @@ public class FirebaseMessageReceiver
     {
         // Pass the intent to switch to the MainActivity
         Intent intent
-                = new Intent(this, MainActivity.class);
+            = new Intent(this, MainActivity.class);
         // Assign channel ID
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel("notification_channel", "FCM Notifications", NotificationManager.IMPORTANCE_HIGH);
+            notificationManager.createNotificationChannel(channel);
+        }
         String channel_id = "notification_channel";
         // Here FLAG_ACTIVITY_CLEAR_TOP flag is set to clear
         // the activities present in the activity stack,
@@ -88,55 +113,55 @@ public class FirebaseMessageReceiver
         // Pass the intent to PendingIntent to start the
         // next Activity
         PendingIntent pendingIntent
-                = PendingIntent.getActivity(
+            = PendingIntent.getActivity(
                 this, 0, intent,
                 PendingIntent.FLAG_ONE_SHOT | PendingIntent.FLAG_IMMUTABLE);
 
         // Create a Builder object using NotificationCompat
         // class. This will allow control over all the flags
         NotificationCompat.Builder builder
-                = new NotificationCompat
-                .Builder(getApplicationContext(),
-                channel_id)
-                .setSmallIcon(R.drawable.savings)
-                .setAutoCancel(true)
-                .setVibrate(new long[] { 1000, 1000, 1000,
-                        1000, 1000 })
-                .setOnlyAlertOnce(true)
-                .setContentIntent(pendingIntent);
+            = new NotificationCompat
+                  .Builder(getApplicationContext(),
+                           channel_id)
+                  .setSmallIcon(R.drawable.savings)
+                  .setAutoCancel(true)
+                  .setVibrate(new long[] { 1000, 1000, 1000,
+                                           1000, 1000 })
+                  .setOnlyAlertOnce(true)
+                  .setContentIntent(pendingIntent);
 
         // A customized design for the notification can be
         // set only for Android versions 4.1 and above. Thus
         // condition for the same is checked here.
         if (Build.VERSION.SDK_INT
-                >= Build.VERSION_CODES.JELLY_BEAN) {
+            >= Build.VERSION_CODES.JELLY_BEAN) {
             builder = builder.setContent(
-                    getCustomDesign(title, message));
+                getCustomDesign(title, message));
         } // If Android Version is lower than Jelly Beans,
         // customized layout cannot be used and thus the
         // layout is set as follows
         else {
             builder = builder.setContentTitle(title)
-                    .setContentText(message)
-                    .setSmallIcon(R.drawable.savings);
+                          .setContentText(message)
+                          .setSmallIcon(R.drawable.savings);
         }
         // Create an object of NotificationManager class to
         // notify the
         // user of events that happen in the background.
-        NotificationManager notificationManager
-                = (NotificationManager)getSystemService(
-                Context.NOTIFICATION_SERVICE);
-        // Check if the Android Version is greater than Oreo
-        if (Build.VERSION.SDK_INT
-                >= Build.VERSION_CODES.O) {
-            NotificationChannel notificationChannel
-                    = new NotificationChannel(
-                    channel_id, "web_app",
-                    NotificationManager.IMPORTANCE_HIGH);
-            notificationManager.createNotificationChannel(
-                    notificationChannel);
-        }
 
-        notificationManager.notify(0, builder.build());
+
+        NotificationManagerCompat notificationManagerCompat = NotificationManagerCompat.from(this);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        notificationManagerCompat.notify(1233, builder.build());
     }
 }
